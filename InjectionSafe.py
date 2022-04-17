f8e012c948a3c019544e8961c41e35b7ef693bba
Ahmad Bajwa <AIB190004@utdallas.edu>
2022-04-17 16:19:50 -0500

    InjectionSafe.py updated variable names

--- InjectionSafe.py
+++ InjectionSafe.py
@@ -1,24 +1,5 @@
 import mysql.connector
-
-
 # Create new entry in Person table
-def add_person():
-    sql = "INSERT INTO person (FirstName, LastName, SSN, Address, DateOfBirth) VALUES (%s, %s, %s, %s, %s)"
-
-    # get user input for each field
-    firstname = input("FirstName: ")
-    lastname = input("LastName: ")
-    ssn = input("SSN: ")
-    address = input("Address: ")
-    dob = input("DateOfBirth [MMDDYYYY]: ")
-
-    val = (firstname, lastname, ssn, address, dob)
-    mycursor.execute(sql, val)
-
-    pharmacydb.commit()
-
-    print(mycursor.rowcount, "record inserted.")
-
 
 def get_person():
     firstname = input("FirstName: ")
@@ -26,24 +7,11 @@ def get_person():
     sql = "SELECT * FROM person WHERE FirstName='%s' AND DateOfBirth='%s'"
     val = (firstname, dob)
 
-    mycursor.execute(sql, val)
-    result = mycursor.fetchall()
+    pharmCursor.execute(sql, val)
+    result = pharmCursor.fetchall()
 
     for x in result:
         print(x)
-
-
-def update_person():
-    firstname = input("Input FirstName: ")
-    current_dob = input("Input Current DateOfBirth [MMDDYYYY]: ")
-    new_dob = input("Input New DateOfBirth [MMDDYYYY]: ")
-    sql = "UPDATE person SET DateOfBirth='" + new_dob + "' WHERE FirstName='" + \
-          firstname + "' AND DateOfBirth='" + current_dob + "'"
-
-
-    mycursor.execute(sql)
-    pharmacydb.commit()
-    print(mycursor.rowcount, "record(s) affected")
 
 
 pharmacydb = mysql.connector.connect(
@@ -55,19 +23,15 @@ pharmacydb = mysql.connector.connect(
 
 print(pharmacydb)
 
-mycursor = pharmacydb.cursor()
+pharmCursor = pharmacydb.cursor()
 
-mycursor.execute("SHOW TABLES")
+pharmCursor.execute("SHOW TABLES")
 
 print("Tables available:")
-for x in mycursor:
+for x in pharmCursor:
     print(x)
 
 while (1 == 1):
-    user_input = input("[1] - Add Person \n[2] - Get Person \n[3] - Update Person \n>")
+    user_input = input("[1] - Get Person\n>")
     if (user_input == '1'):
-        add_person()
-    elif (user_input == '2'):
         get_person()
-    elif (user_input == '3'):
-        update_person()
