b5e96228355c614d0fed3664243a11dcf1277f38
AhmadBajwaCS <AIB190004@utdallas.edu>
2022-04-21 08:06:34 -0500

    Slight Changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,25 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Pharmacy DB</h1>
+        <h1>Pharmacy DB  &emsp;  &ensp;    </h1>
         <button class="button">Search the database</button>
         <button class="button">Log prescription</button>
         <button class="button">Track active prescriptions</button>
         <button class="button">View drug inventory</button>
         <button class="button">Add new customer</button>
         <button class="button">Quit</button>
-        <p>
-          Pharmacy DB
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
     </div>
   );
 }
