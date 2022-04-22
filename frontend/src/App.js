f22ef181f2f87b0aa02b6da3d77ceee87922037a
avincent428 <amielvincent@gmail.com>
2022-04-22 14:59:45 -0500

    added router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Table from './components/Table'
-import {Route} from "react-router-dom"
+import HomePageButtons from './components/HomePageButtons'
+import {Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <div className="App-body">
-       
-        
-        <Route path="/" render={() => {
-          return (
-             <h1>Pharmacy DB  &emsp;  &ensp;    </h1>
-        <button className="button">Search the database</button>
-        <button className="button">Log prescription</button>
-        <button className="button">Track active prescriptions</button>
-        <button className="button">View drug inventory</button>
-        <button className="button">Add new customer</button>
-        <button className="button">Quit</button>
-          );
-        }} />
-        
 
-      </div>
+        <Route path="/" exact component={HomePageButtons} />
     </div>
   );
 }
