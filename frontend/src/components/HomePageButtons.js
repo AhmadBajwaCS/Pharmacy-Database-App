ef049a5c0fb40f11c6ee27c2511f6ea1e22d1790
Ahmad Bajwa <AIB190004@utdallas.edu>
2022-04-22 16:51:55 -0500

    Restyle App and new pages

--- frontend/src/components/HomePageButtons.js
+++ frontend/src/components/HomePageButtons.js
@@ -5,14 +5,12 @@ function HomePageButtons(props) {
     return (
         <div className="homeDiv">
         <h1>&emsp;  Pharmacy DB  &emsp;  </h1>
-            <div className="buttonsMenu">
                 <button className="button">Search the database</button>
                 <button className="button">Log prescription</button>
                 <button className="button">Track active prescriptions</button>
                 <button className="button">View drug inventory</button>
                 <button className="button">Add new customer</button>
                 <button className="button">Quit</button>
-            </div>
         </div>
     );
 }
