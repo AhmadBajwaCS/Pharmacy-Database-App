e7cf2c579f9ced836c7c84fb9123f02721326c88
Henry W <htwerner01@gmail.com>
2022-04-23 15:58:00 -0500

    Import new font

--- server/index.js
+++ server/index.js
@@ -9,13 +9,31 @@ app.use(express.json())
 
 // Route to get all people
 app.get("/api/get", (req, res) => {
-    db.query("SELECT * FROM person", (err, result) => {
-        if (err) {
-            console.log(err)
-        }
-        res.send(result)
-    });
+    db.query("SELECT * FROM person",
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            }
+            res.send(result)
+        });
 });
+
+// Route to add new person
+app.post('/api/create', (req, res) => {
+    const fn = req.body.FirstName;
+    const ln = req.body.LastName;
+    const ssn = req.body.SSN;
+    const address = req.body.Address;
+    const dob = req.body.DateOfBirth
+
+    db.query("INSERT INTO person (FirstName, LastName, SSN, Address, DateOfBirth) VALUES (?,?,?,?,?)", [fn, ln, ssn, address, dob],
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            }
+            console.log(result)
+        });
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
