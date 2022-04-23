4d5f0fa540f8bb85a661d693019a53743dde2462
avincent428 <amielvincent@gmail.com>
2022-04-22 22:08:21 -0500

    buttons work kind of

--- frontend/src/components/HomePageButtons.js
+++ frontend/src/components/HomePageButtons.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import './HomePageButtons.css';
+import React from "react";
+import "./HomePageButtons.css";
+import { Link } from "react-router-dom";
 
 function HomePageButtons(props) {
-    return (
-        <div className="homeDiv">
-        <h1>&emsp;  Pharmacy DB  &emsp;  </h1>
-                <button className="button">Search the database</button>
-                <button className="button">Log prescription</button>
-                <button className="button">Track active prescriptions</button>
-                <button className="button">View drug inventory</button>
-                <button className="button">Add new customer</button>
-                <button className="button">Quit</button>
-        </div>
-    );
+  return (
+    <div className="homeDiv">
+      <h1>&emsp; Pharmacy DB &emsp; </h1>
+      <Link to="/search">
+        <button className="button">Search the database</button>
+      </Link>
+      <Link to="/log">
+        <button className="button">Log prescription</button>
+      </Link>
+      <Link to="/tracker">
+        <button className="button">Track active prescriptions</button>
+      </Link>
+      <Link to="/drug-inventory">
+        <button className="button">View drug inventory</button>
+      </Link>
+      <Link to="/new">
+        <button className="button">Add new customer</button>
+      </Link>
+      <button className="button">Quit</button>
+    </div>
+  );
 }
 
 export default HomePageButtons;
