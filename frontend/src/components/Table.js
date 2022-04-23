1e4cada1fe470d36c383275f8ed073e008e9e677
avincent428 <amielvincent@gmail.com>
2022-04-23 13:26:25 -0500

    added table

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React from "react";
+import "./Table.css";
 
 function Table(props) {
-    return (
-        <div className="table">
-            This is the table
-        </div>
-    );
+  return (
+    <div className="content-table">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Date of Birth</th>
+          <th>Address</th>
+          <th>SSB</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>John Doe</td>
+          <td>4-6-89</td>
+          <td>2014 Forest Hills Drive</td>
+          <td>XXX-XX-XXXX</td>
+        </tr>
+        <tr>
+          <td>John Doe</td>
+          <td>4-6-89</td>
+          <td>2014 Forest Hills Drive</td>
+          <td>XXX-XX-XXXX</td>
+        </tr>
+        <tr>
+          <td>John Doe</td>
+          <td>4-6-89</td>
+          <td>2014 Forest Hills Drive</td>
+          <td>XXX-XX-XXXX</td>
+        </tr>
+      </tbody>
+    </div>
+  );
 }
 
 export default Table;
