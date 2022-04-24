acbb31dea249c9f096d368eb6ca5a8f7fcdbc567
Henry W <htwerner01@gmail.com>
2022-04-23 21:10:05 -0500

    Add new customer functionality

--- frontend/src/components/AddNewCustomer.js
+++ frontend/src/components/AddNewCustomer.js
@@ -13,29 +13,82 @@ function AddNewCustomer(props) {
         ()=>{Axios.get("http://localhost:3002/api/get").then((data)=>{setPostList(data.data)});},
         [])
 
+
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [ssn, setSsn] = useState("");
+    const [address, setAddress] = useState("");
+    const [dateOfBirth, setDateOfBirth] = useState(new Date()); // TODO: make sure this is the correct state
+
+    const submitCustomer = () => {
+        Axios.post('http://localhost:3002/api/create', {
+            FirstName: firstName,
+            LastName: lastName,
+            SSN: ssn,
+            Address: address,
+            DateOfBirth: dateOfBirth
+        })
+    }
+
     return (
-        <div className="newCus">
-            {postList.map((val,key)=>{
-                return (
-                <div className="Post">
-                    <h2>Person</h2>
-                    <p>{val.FirstName}</p>
-                    <p>{val.LastName}</p>
-                </div>
-                )  
-            })}
-            <form>
-                First Name:<br></br>
-                    <input type="text" name="fname" /> <br></br>
-                Last Name:  <br></br>
-                    <input type="text" name="lname" /> <br></br>
-                Date of Birth:<br></br>
-                <input type="text" name="dob" /> <br></br>
-                Address  <br></br>
-                <input type="text" name="address" /> <br></br>
-                SSN  <br></br>
-                <input type="text" name="ssn" /> <br></br>
-            </form>
+        <div>
+            <div className="newCus">
+                
+                <form>
+                    <label>First Name: </label>
+                    <input 
+                        type="text" 
+                        name="fnamein"
+                        onChange={(event) => {setFirstName(event.target.value);}}
+                    />
+                    <br></br>
+
+                    <label>Last Name: </label>
+                    <input 
+                        type="text" 
+                        name="lnamein" 
+                        onChange={(event) => {setLastName(event.target.value);}}
+                    />
+                    <br></br>
+                    
+                    <label>Date of Birth: </label>
+                    <input 
+                        type="text" 
+                        name="dobin" 
+                        onChange={(event) => {setDateOfBirth(event.target.value);}}
+                    />
+                    <br></br>
+
+                    <label>Address:</label>
+                    <input 
+                        type="text" 
+                        name="addressin" 
+                        onChange={(event) => {setAddress(event.target.value);}}
+                    />
+                    <br></br>
+                    
+                    <label>SSN:</label>
+                    <input 
+                        type="text" 
+                        name="ssnin" 
+                        onChange={(event) => {setSsn(event.target.value);}}
+                    />
+                    <br></br>
+                    <br></br>
+                    
+                    <div>
+                        <button onClick={submitCustomer}>ADD CUSTOMER</button>
+                    </div>
+                </form>
+                
+            </div>
+            <div className='currentCustomerList'>
+                {postList.map((val,key)=>{ return (
+                    <div className="Post">
+                        <p>{val.FirstName} {val.LastName}  |  {val.SSN}  |  {val.Address}  |  {val.DateOfBirth}</p>
+                    </div>
+                )})}
+            </div>
         </div>
     );
 }
