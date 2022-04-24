7152fdfb37cfe917b2407ca06e3579878690b337
avincent428 <amielvincent@gmail.com>
2022-04-24 18:31:35 -0500

    moved table

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import HomePageButtons from './components/HomePageButtons'
-import AddNewCustomer from './components/AddNewCustomer'
-import LogPrescription from './components/LogPrescription'
-import Table from './components/Table'
-import {Route} from 'react-router-dom'
-import Quit from './components/Quit'
+import logo from "./logo.svg";
+import "./App.css";
+import HomePageButtons from "./components/HomePageButtons";
+import AddNewCustomer from "./components/AddNewCustomer";
+import LogPrescription from "./components/LogPrescription";
+import Table from "./components/Table";
+import { Route } from "react-router-dom";
+import Quit from "./components/Quit";
 
 function App() {
   return (
-      <body>
-              <div>
-
-                  <Route path="/" exact component={HomePageButtons} />
-                  <Route path="/new" exact component={AddNewCustomer} />
-                  <Route path="/log" exact component={LogPrescription} />
-                  <Route path="/table" exact component={Table} />
-                  <Route path="/quit" exact component={Quit} />
-
-              </div>
-
-      </body>
+    <body>
+      <div>
+        <Route path="/" exact component={HomePageButtons} />
+        <Route path="/new" exact component={AddNewCustomer} />
+        <Route path="/log" exact component={LogPrescription} />
+        <Route path="/search" exact component={Table} />
+        <Route path="/quit" exact component={Quit} />
+      </div>
+    </body>
   );
 }
 
